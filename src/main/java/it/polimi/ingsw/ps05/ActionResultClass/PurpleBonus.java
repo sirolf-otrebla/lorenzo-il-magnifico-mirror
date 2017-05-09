6f5412dc6865831f213f3e002656ccef03be3e09
lucafalaschini <lucafalaschini@10.169.219.244>
2017-05-09 15:19:08 +0200

    Aggiunte tutte le classi dell'interfaccia ActionResult (prova)

---
+++ src/main/java/it/polimi/ingsw/ps05/ActionResultClass/PurpleBonus.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.ps05.ActionResultClass;
+
+public class PurpleBonus implements ActionResult {
+
+}
---
+++ src/main/java/it/polimi/ingsw/ps05/ActionResultClass/YellowBonus.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.ps05.ActionResultClass;
+
+public class YellowBonus implements ActionResult {
+
+}
