88c1f329af8ab7b51a02164d18fae8cace45e3ee
Alberto <alberto.floris@mail.polimi.it>
2017-05-10 12:20:03 +0200

    aggiunte classi di modello preliminari ( in particolare classi sugli spazi del tabellone)

---
+++ src/main/java/it/polimi/ingsw/ps05/model/ActionSpace.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.ps05.model;
+
+public abstract class ActionSpace {
+
+}
---
+++ src/main/java/it/polimi/ingsw/ps05/model/Board.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.ps05.model;
+
+/* See UML for description
+ * 
+ * 
+ */
+public class Board {
+
+}
