7d86ffec90fc1f8f1880c1d764dba15ded0b698d
“lucafala” <luca.falaschini@mail.polimi.it>
2017-05-10 20:21:46 +0200

    Elenco carte completo

---
+++ src/main/java/it/polimi/ingsw/ps05/ResourcesAndBonuses/BonusWithMultiplier.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.ps05.ResourcesAndBonuses;
+
+public class BonusWithMultiplier implements ActionResult {
+	public int multiplier;
+	//public Resource resource --> classe della risorsa che da in output (Gold o Victory)
+	//public Colour colour --> colore delle carte da contare !!! si possono contare anche i punti militare
+}
