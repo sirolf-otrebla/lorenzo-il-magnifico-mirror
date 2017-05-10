f64ed5cdc6b6a89530b5c6c82affd839343e9ece
andreami96 <andreami96@gmail.com>
2017-05-10 21:13:02 +0200

    aggiunto turno, azione e interfaccia periodo

---
+++ src/main/java/it/polimi/ingsw/ps05/model/Turn.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.ps05.model;
+
+import java.util.ArrayList;
+
+public class Turn implements Period {
+	
+	ArrayList<Player> Order; // turn order
+	Turn next; 
+	boolean FinalTurn;
+	
+	// instantiate three Dice objects with random values
+	public void lanciaDadi(){
+		//TODO
+	}
+}
