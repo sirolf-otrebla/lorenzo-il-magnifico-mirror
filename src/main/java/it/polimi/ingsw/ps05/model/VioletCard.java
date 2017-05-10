690597aed8468e45f3c75530b2d1d151c73a6e5b
Alberto <alberto.floris@mail.polimi.it>
2017-05-10 21:52:55 +0200

    sync

---
+++ src/main/java/it/polimi/ingsw/ps05/model/VioletCard.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.ps05.model;
+
+import java.util.ArrayList;
+
+import it.polimi.ingsw.ps05.ResourcesAndBonuses.Resource;
+
+public class VioletCard extends TowerCard  {
+	
+	public VioletCard(Epoch epoch, Color color, String cardName, ArrayList<ArrayList<Resource>> effects) {
+		super(epoch, color, cardName, effects);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public VioletCard(Epoch epoch, Color color, String cardName,  ArrayList<ArrayList<Resource>> requirements,
+			ArrayList<ArrayList<Resource>> effects) {
+		super(epoch, color, cardName, requirements, effects);
+	}
+
+}
---
+++ src/main/java/it/polimi/ingsw/ps05/model/YellowCard.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.ps05.model;
+
+import java.util.ArrayList;
+
+import it.polimi.ingsw.ps05.ResourcesAndBonuses.Resource;
+
+public class YellowCard extends TowerCard  {
+
+	public YellowCard(Epoch epoch, Color color, String cardName, ArrayList<ArrayList<Resource>> effects) {
+		super(epoch, color, cardName, effects);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public YellowCard(Epoch epoch, Color color, String cardName,  ArrayList<ArrayList<Resource>> requirements,
+			ArrayList<ArrayList<Resource>> effects) {
+		super(epoch, color, cardName, requirements, effects);
+	}
+
+}
