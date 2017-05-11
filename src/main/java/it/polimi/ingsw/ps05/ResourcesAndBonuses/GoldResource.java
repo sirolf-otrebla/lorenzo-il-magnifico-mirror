fabbcdc5bce4b8b98dbadddffe9a741d26e46167
lucaf <lucaf@DESKTOP-6EHQ1DS.lan>
2017-05-11 20:59:26 +0200

    Aggiunte variabili a classi, test per il parser

--- src/main/java/it/polimi/ingsw/ps05/ResourcesAndBonuses/GoldResource.java
+++ src/main/java/it/polimi/ingsw/ps05/ResourcesAndBonuses/GoldResource.java
@@ -1,5 +1,21 @@
 package it.polimi.ingsw.ps05.ResourcesAndBonuses;
 
 public class GoldResource implements Resource {
-
+	private int amount;
+	
+	public GoldResource(int amount){
+		this.amount = amount;
+	}
+	
+	public GoldResource() {
+		
+	}
+	
+	public void setAmount(int amount){
+		this.amount = amount;
+	}
+	
+	public int getAmount(){
+		return this.amount;
+	}
 }
--- src/main/java/it/polimi/ingsw/ps05/ResourcesAndBonuses/MilitaryResource.java
+++ src/main/java/it/polimi/ingsw/ps05/ResourcesAndBonuses/MilitaryResource.java
@@ -1,5 +1,21 @@
 package it.polimi.ingsw.ps05.ResourcesAndBonuses;
 
 public class MilitaryResource implements Resource {
-
+	private int amount;
+	
+	public MilitaryResource(int amount){
+		this.amount = amount;
+	}
+	
+	public MilitaryResource() {
+		
+	}
+	
+	public void setAmount(int amount){
+		this.amount = amount;
+	}
+	
+	public int getAmount(){
+		return this.amount;
+	}
 }
