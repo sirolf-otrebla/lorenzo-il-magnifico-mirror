97ed5a5cae0dfefd1f0733e75856ca0113453618
andreami96 <andreami96@gmail.com>
2017-05-14 13:02:25 +0200

    piccole modifiche lessicali

--- src/main/java/it/polimi/ingsw/ps05/model/Turn.java
+++ src/main/java/it/polimi/ingsw/ps05/model/Turn.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class Turn implements Period {
 	
-	ArrayList<Player> Order; // turn order
+	ArrayList<Player> order; // turn order
 	Turn next; 
-	boolean FinalTurn;
+	boolean finalTurn;
 	
 	// instantiate three Dice objects with random values
 	public void lanciaDadi(){
