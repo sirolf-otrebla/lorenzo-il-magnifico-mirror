25bbf6d20a1885579968c73e3db48423d92c3f44
sirolf-otrebla <albeflor@gmail.com>
2017-06-27 17:14:40 +0200

    modifiche client

---
+++ src/main/java/it/polimi/ingsw/ps05/client/view/gui/ActionSpaceWidget.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.ps05.client.view.gui;
+
+import javafx.scene.shape.Circle;
+
+/**
+ * Created by Alberto on 27/06/2017.
+ */
+public class ActionSpaceWidget {
+
+    public Circle familyMemberCircle;
+
+}
