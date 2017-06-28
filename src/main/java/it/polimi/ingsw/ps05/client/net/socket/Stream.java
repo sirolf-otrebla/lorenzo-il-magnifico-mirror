b2b49a2b571375788098f9ff2cdd41c252fdf516
lucafala <luca.falaschini@mail.polimi.it>
2017-06-28 13:38:08 +0200

    sync

---
+++ src/main/java/it/polimi/ingsw/ps05/client/net/socket/Stream.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.ps05.client.net.socket;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import it.polimi.ingsw.ps05.net.message.NetMessage;
+
+public class Stream {
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	private Socket socket;
+
+	public Stream(Socket socket) throws IOException{
+		this.socket = socket;
+		out = new ObjectOutputStream(this.socket.getOutputStream());
+		out.flush();
+		in = new ObjectInputStream(this.socket.getInputStream());
+	}
+
+	public NetMessage takeInData() throws ClassNotFoundException, IOException{
+		return (NetMessage)in.readObject();
+	}
+
+	public void sendData(NetMessage obj) throws IOException{
+		out.writeObject(obj);
+	}
+}
