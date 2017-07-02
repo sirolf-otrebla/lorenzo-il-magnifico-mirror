28be8c8d8fb1e670458ce288f7e3adf28bac8b5d
sirolf-otrebla <albeflor@gmail.com>
2017-07-02 18:32:16 +0200

    Completato DraftController Carte Leader

---
+++ src/main/java/it/polimi/ingsw/ps05/net/message/EndDraftMessage.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.ps05.net.message;
+
+import it.polimi.ingsw.ps05.server.net.NetMessageVisitor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alberto on 02/07/2017.
+ */
+public class EndDraftMessage implements LeaderDraftMessage {
+
+    private ArrayList<Integer> playerLeaderCards;
+
+    public EndDraftMessage(ArrayList<Integer> playerLeaderCards){
+
+        this.playerLeaderCards = playerLeaderCards;
+    }
+
+    @Override
+    public void acceptVisitor(NetMessageVisitor vi) throws Exception {
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/ps05/net/message/LeaderDraftMessage.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.ps05.net.message;
+
+/**
+ * Created by Alberto on 02/07/2017.
+ */
+public interface LeaderDraftMessage extends NetMessage {
+}
