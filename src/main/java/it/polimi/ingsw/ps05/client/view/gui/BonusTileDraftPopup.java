df97f0eabbad177d4557d0bc49e9cc56d34a4e95
andreami96 <andreami96@gmail.com>
2017-07-10 16:50:10 +0200

    GUI bonus tile draft update

--- src/main/java/it/polimi/ingsw/ps05/client/view/gui/BonusTileDraftPopup.java
+++ src/main/java/it/polimi/ingsw/ps05/client/view/gui/BonusTileDraftPopup.java
@@ -2,9 +2,11 @@ package it.polimi.ingsw.ps05.client.view.gui;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -25,8 +27,10 @@ public class BonusTileDraftPopup {
 
     private static Stage popupStage;
     private static BonusTileWidget[] bonusTileArray = new BonusTileWidget[4];
-
+    private static BonusTileWidget[] newBonusTileArray;
+    private static boolean myTurn;
     private static BonusTileWidget selectedBonusTile;
+    private static HBox hboxReference;
 
     public static void display() {
 
@@ -35,12 +39,14 @@ public class BonusTileDraftPopup {
         popupStage.initStyle(StageStyle.UNDECORATED);
 
         HBox hbox = new HBox(50 * resize);
+        hboxReference = hbox;
         VBox vbox = new VBox(40 * resize);
         GridPane gridPane = new GridPane();
 
         // create bonusTile widgets
         for(int i = 0; i < 4; i++) {
             bonusTileArray[i] = new BonusTileWidget(i);
+            bonusTileArray[i].setOpacity(0.3);
             System.out.println(bonusTileArray[i].getImagePath());
             hbox.getChildren().add(bonusTileArray[i]);
             //GridPane.setConstraints(bonusTileArray[i].getImage(), i, 0);
@@ -81,12 +87,24 @@ public class BonusTileDraftPopup {
 
     }
 
-    public void updateDraft(int draftedBonusTile) {
-        bonusTileArray[draftedBonusTile].setDrafted(true);
-        repaint();
+    public static void updateDraft(Integer[] bonusTileToDraftArray) {
+
+        int j = 0;
+        for(int i = 0; i < 4; i++) {
+            bonusTileArray[i].setDrafted(true);
+            for(Integer bonusTileToDraft: bonusTileToDraftArray) {
+                if (bonusTileArray[i].getReferenceId() == bonusTileToDraft) {
+                    bonusTileArray[i].setMouseTransparent(false);
+                    bonusTileArray[i].setOpacity(1);
+                    bonusTileArray[i].setDrafted(false);
+                    setupClickGesture(bonusTileArray[i]);
+                }
+            }
+        }
     }
 
-    private void repaint() {
+    /*
+    private static void repaint() {
 
         for(BonusTileWidget bonusTile: bonusTileArray) {
             if(!bonusTile.isDrafted()) {
@@ -97,6 +115,21 @@ public class BonusTileDraftPopup {
             }
         }
     }
+    */
+
+    private static void setupClickGesture(BonusTileWidget bonusTileToDraft) {
+
+        bonusTileToDraft.setOnMouseEntered((MouseEvent e) -> {
+            bonusTileToDraft.setCursor(Cursor.HAND);
+        });
+
+        bonusTileToDraft.setOnMouseClicked((MouseEvent e) -> {
+            bonusTileToDraft.setDrafted(true);
+            bonusTileToDraft.setThisPlayerSelected(true);
+            //TODO comuicare al controller
+        });
+
+    }
 
     public static BonusTileWidget getSelectedBonusTile() {
         return selectedBonusTile;
@@ -104,5 +137,13 @@ public class BonusTileDraftPopup {
 
     public static Stage getPopupStage() {
         return popupStage;
+    }
+
+    public static boolean isMyTurn() {
+        return myTurn;
+    }
+
+    public static void setMyTurn(boolean myTurn) {
+        BonusTileDraftPopup.myTurn = myTurn;
     }
 }
